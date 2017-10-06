2697550acd061c3fec7bec566158fee2cc2dfb82
Vikram Voleti <vikky2904@gmail.com>
2017-10-06 11:47:17 +0530

    Improved params

--- params.py
+++ params.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import sys
 
 #############################################################
 # PARAMS TO BE SET
@@ -11,6 +12,23 @@ saveDir = rootDir
 # # saveDir = '/Neutron6/voleti.vikram/GRIDcorpusResults/1-h32-d1-Adam-2e-4-s0107-s0920-s2227'
 # saveDir = rootDir
 
+
+#############################################################
+# IMPORT
+#############################################################
+
+if 'ROOT_DIR' not in dir():
+	ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+if ROOT_DIR not in sys.path:
+    sys.path.append(ROOT_DIR)
+
+
+if 'LIPREADER_DIR' not in dir():
+    LIPREADER_DIR = os.path.join(ROOT_DIR, 'lipreader')
+
+if LIPREADER_DIR not in sys.path:
+    sys.path.append(LIPREADER_DIR)
 
 #############################################################
 # TRUE PARAMS
@@ -53,3 +71,21 @@ currDir = os.path.dirname(os.path.realpath(__file__))
 
 # wordIdx dictionary
 wordIdx = np.load(os.path.join(currDir, "wordIdx.npy")).item()
+
+# grid_vocab list
+GRID_VOCAB_LIST_FILE = os.path.join(ROOT_DIR, 'grid_vocabulary.txt')
+
+
+#############################################################
+# LOAD VOCAB LIST
+#############################################################
+
+def load_gridcorpus_vocab_list(GRID_VOCAB_LIST_FILE):
+    grid_vocab = []
+    with open(GRID_VOCAB_LIST_FILE) as f:
+        for line in f:
+            word = line.rstrip().split()[-1]
+            grid_vocab.append(word)
+    return grid_vocab
+
+GRID_VOCAB = load_gridcorpus_vocab_list(GRID_VOCAB_LIST_FILE)
